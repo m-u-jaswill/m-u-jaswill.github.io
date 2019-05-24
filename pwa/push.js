d27d997104db4f5f1b94d2bba54efaa8e16c51ab
masahiro-ueda <masahiro-ueda@jaswill.com>
2019-05-24 17:15:02 +0900

    17:14

--- pwa/push.js
+++ pwa/push.js
@@ -5,7 +5,7 @@ const push = require('web-push');
 const GCM_API_KEY = 'AAAAJx7C8uA:APA91bGH7YHzV8Wxp4H5IYGUDcIJ3yfM20nkZau-cN5sCbbzgmKa46y2aolu9VKrEefvpdj2TqxuYx7C0ZJvPAexofjg5iaVKUTQtV9uSj-gNdOjix1QDyrnN-AFhs8dFseuRiZ2160S'
 push.setGCMAPIKey(GCM_API_KEY);
 
-const data = {
+const data = { 
 	'endpoint': 'https://fcm.googleapis.com/fcm/send/fozdC9Frnm8:APA91bF2Z4OkW2uOgQzSeEZ8ABkEcV5Q7WKwsvDA4Xjoi-tNBNu6BbL_gALrKvVyxY6iXSn3zpB-wpTgP0XtUJADsJCl4vgTlO8a4hEk1PWPPdO7ZecuvVa2jn43rgN92-xNfEbZ2jh1',
 	'userAuth': 'uMHThb95P6rWJH3HwqgYPg==',
 	'userPublicKey': 'BJVrSr/PDZYKYyrrboSHxsnyrvpUeje6GezMrOHBgEVg+XwL1qpH6skA8YpnKG8E6dkJzDac8VIWRkiazxPwfrw='
